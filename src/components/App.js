6ed8b767a67e7230fa7cd475d9e118434d0c4db5
Aleksei Bulgakov <contact@alexbulgakov.ru>
2022-03-22 00:51:42 +0700

    feat: вынес компонент Header

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,19 @@
-import './index.css';
-import logo from './images/logo.svg';
-import pen from './images/pen.svg';
-import plus from './images/plus.svg';
+import Header from './Header';
+import '../index.css';
+import pen from '../images/pen.svg';
+import plus from '../images/plus.svg';
 
 function App() {
   return (
     <body>
       <div className="root">
-        <header className="header">
-          <img className="header__logo" src={logo} alt="Логотип"></img>
-        </header>
+        <Header />
         <main className="content">
           <section class="profile">
             <div className="profile__avatar-box">
               <img className="profile__avatar" src="<%=require('./images/photo.jpg')%>" alt="Аватар"></img>
-                <button type="button" className="profile__avatar-btn"><img className="profile__avatar-edit-img"
-                  src="<%=require('./images/pen.svg')%>" alt="Кнопка редактирования"></img></button>
+              <button type="button" className="profile__avatar-btn"><img className="profile__avatar-edit-img"
+                src="<%=require('./images/pen.svg')%>" alt="Кнопка редактирования"></img></button>
             </div>
             <div className="profile__profile-info">
               <h1 className="profile__name">Жак-Ив Кусто</h1>
@@ -43,13 +41,13 @@ function App() {
                 <label className="popup-form__field">
                   <input className="popup-form__item popup-form__item_el_name" type="text" name="name"
                     id="name-input" required minlength="2" maxlength="40" placeholder="Имя"></input>
-                    <span class="popup-form__input-error name-input-error"></span>
+                  <span class="popup-form__input-error name-input-error"></span>
                 </label>
                 <label className="popup-form__field">
                   <input className="popup-form__item popup-form__item_el_about" type="text" name="about"
                     id="about-input" required minlength="2" maxlength="200"
                     placeholder="Профессиональная деятельность"></input>
-                    <span className="popup-form__input-error about-input-error"></span>
+                  <span className="popup-form__input-error about-input-error"></span>
                 </label>
                 <button className="popup-form__button" type="submit">Сохранить</button>
               </fieldset>
@@ -66,12 +64,12 @@ function App() {
                 <label className="popup-form__field">
                   <input className="popup-form__item popup-form__item_el_name" type="text" name="name"
                     placeholder="Название" id="name" required minlength="2" maxlength="30"></input>
-                    <span className="popup-form__input-error name-error"></span>
+                  <span className="popup-form__input-error name-error"></span>
                 </label>
                 <label className="popup-form__field">
                   <input className="popup-form__item popup-form__item_el_link" type="url" name="link"
                     placeholder="Ссылка на картинку" id="link" required></input>
-                    <span className="popup-form__input-error link-error"></span>
+                  <span className="popup-form__input-error link-error"></span>
                 </label>
                 <button className="popup-form__button" type="submit">Создать</button>
               </fieldset>
@@ -88,7 +86,7 @@ function App() {
                 <label className="popup-form__field">
                   <input className="popup-form__item popup-form__item_avatar_link" type="url" name="link-avatar"
                     placeholder="Ссылка на аватар" id="avatar-link" required></input>
-                    <span className="popup-form__input-error avatar-link-error"></span>
+                  <span className="popup-form__input-error avatar-link-error"></span>
                 </label>
                 <button className="popup-form__button" type="submit">Сохранить</button>
               </fieldset>
@@ -112,7 +110,7 @@ function App() {
           <div className="popup__container-img">
             <button className="popup__close-icon" type="button"></button>
             <img className="popup__picture" src="#" alt="Фото локации"></img>
-              <p className="popup__location-name"></p>
+            <p className="popup__location-name"></p>
           </div>
         </section>
 
@@ -124,14 +122,14 @@ function App() {
       <template id="card">
         <li className="element">
           <img className="element__picture" src="#" alt="Фото локации"></img>
-            <button className="element__delete" type="button"></button>
-            <div className="element__location-box">
-              <h2 className="element__location"></h2>
-              <div className="element__like-box">
-                <button className="element__like" type="button"></button>
-                <span className="element__likes-counter"></span>
-              </div>
+          <button className="element__delete" type="button"></button>
+          <div className="element__location-box">
+            <h2 className="element__location"></h2>
+            <div className="element__like-box">
+              <button className="element__like" type="button"></button>
+              <span className="element__likes-counter"></span>
             </div>
+          </div>
         </li>
       </template>
     </body>
