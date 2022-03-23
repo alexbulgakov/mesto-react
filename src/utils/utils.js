1a19a705b056d408d68ce791e6768d22de508b03
Aleksei Bulgakov <contact@alexbulgakov.ru>
2022-03-24 03:52:32 +0700

    feat: портировал API

---
+++ src/utils/utils.js
@@ -0,0 +1,8 @@
+export const options = {
+    baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-26',
+    headers: {
+        authorization: 'de3be763-36f0-4323-96ef-5a3ebe72ac8d',
+        'Content-Type': 'application/json'
+    }
+};
+
